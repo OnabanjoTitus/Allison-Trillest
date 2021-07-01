c0f665df94e73e2e5e62bbc8671533ecffc52d74
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-07-01 12:15:00 +0100

    first commit

---
+++ src/main/java/com/trillestojay/trillest/TrillestApplication.java
@@ -0,0 +1,13 @@
+package com.trillestojay.trillest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrillestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrillestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trillestojay/trillest/TrillestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trillestojay.trillest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrillestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
