5847e87a127fe8afd4ab0c67ac9065b843ffdcb0
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-07-07 18:44:07 +0100

    updates

--- src/main/java/com/trillestojay/trillest/web/ProductController.java
+++ src/main/java/com/trillestojay/trillest/web/ProductController.java
@@ -1,7 +1,14 @@
 package com.trillestojay.trillest.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("v1")
 public class ProductController {
+    @GetMapping("")
+    public String testConnection(){
+        return "We are On";
+    }
 }
